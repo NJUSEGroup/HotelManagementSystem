e2ecf570e73f6d2e7696dda377f3ebafcc5ff20e
root <root@172.26.90.41>
2016-12-06 10:38:24 +0800

    12/6 pull前的push

--- HotelReservationSystem-parent/HotelReservationSystem-client/src/main/java/hrs/client/UI/WebMarketUI/WebDiscountUI/WebDiscountListener/ModifyMouseListener.java
+++ HotelReservationSystem-parent/HotelReservationSystem-client/src/main/java/hrs/client/UI/WebMarketUI/WebDiscountUI/WebDiscountListener/ModifyMouseListener.java
@@ -1,5 +1,56 @@
 package hrs.client.UI.WebMarketUI.WebDiscountUI.WebDiscountListener;
 
-public class ModifyMouseListener {
+import java.awt.Color;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+import javax.swing.JButton;
+
+import hrs.client.UI.WebMarketUI.WebDiscountUI.WebDiscountPanel;
+import hrs.client.util.UIConstants;
+
+public class ModifyMouseListener implements MouseListener{
+	private JButton jbButton;
+	private WebDiscountPanel webDiscountPanel;
+	public ModifyMouseListener(WebDiscountPanel webDiscountPanel) {
+		// TODO Auto-generated constructor stub
+		this.webDiscountPanel=webDiscountPanel;
+	}
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+		webDiscountPanel.showModifyDialog();
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		jbButton = (JButton)e.getSource();
+		//鼠标在标签上时更换背景及字体色
+		jbButton.setBackground(Color.WHITE);
+		jbButton.setForeground(Color.BLACK);
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		jbButton = (JButton)e.getSource(); 
+		//鼠标离开标签时更换背景及字体色
+		 jbButton.setBackground(UIConstants.JBUTTON_BLUE);
+		 jbButton.setForeground(Color.white);
+	}
+	
 
 }
--- HotelReservationSystem-parent/HotelReservationSystem-client/src/main/java/hrs/client/util/UIConstants.java
+++ HotelReservationSystem-parent/HotelReservationSystem-client/src/main/java/hrs/client/util/UIConstants.java
@@ -13,9 +13,4 @@ public class UIConstants {
 	
 	final public static Font JZONE_FONT=new Font("Arial Unicode MS", Font.PLAIN, 25);
 	final public static Font JLABEL_FONT=new Font("Arial Unicode MS", Font.PLAIN, 21);
-	final public static Font jzoneChinese=new Font("Arial Unicode MS", Font.PLAIN, 25);
-	final public static Font jlabelChinese=new Font("Arial Unicode MS", Font.PLAIN, 21);
-	final public static Color jframe=new Color(211,237,249);
-	final public static Color jlabel=new Color(0,160,233);
-
 }
